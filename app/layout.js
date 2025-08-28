e1eee969d866f8a526d1d5ccb91b33e3ea37fe2d
Kittimaeve11 <Kittima.eve11@gmail.com>
2025-08-28 10:24:31 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,41 +1,51 @@
 import { LocaleProvider } from './Context/LocaleContext';
+import './globals.css';
+import localFont from 'next/font/local';
 import Navbartest from './components/Navbartest';
 import TabMenu from './components/TabMenu';
 import Footer from './components/Footer';
 import BackToTopButton from './components/BackToTopButton';
-import './globals.css';
 import FloatingButtons from './components/FloatingButtons';
 import ToastProvider from './components/ToastProvider';
-import ClientConditionalLayout from './components/ClientConditionalLayout'; // <-- ใส่ไว้
-
-import localFont from 'next/font/local';
+import CookieBanner from './components/CookieBanner';
+import GoogleAnalytics from './components/GoogleAnalytics';
 
 const sukhumvitTadmai = localFont({
   src: [
-    { path: '../public/fonts/SukhumvitTadmai-UltraLight.otf', weight: '200', style: 'normal' },
-    { path: '../public/fonts/SukhumvitTadmai-Text.otf', weight: '400', style: 'normal' },
-    { path: '../public/fonts/SukhumvitTadmai-SemiBold.otf', weight: '600', style: 'normal' },
-    { path: '../public/fonts/SukhumvitTadmai-ExtraBold.otf', weight: '800', style: 'normal' },
+    { path: '/fonts/SukhumvitTadmai-UltraLight.otf', weight: '200', style: 'normal' },
+    { path: '/fonts/SukhumvitTadmai-Text.otf', weight: '400', style: 'normal' },
+    { path: '/fonts/SukhumvitTadmai-SemiBold.otf', weight: '600', style: 'normal' },
+    { path: '/fonts/SukhumvitTadmai-ExtraBold.otf', weight: '800', style: 'normal' },
   ],
   variable: '--font-sukhumvit',
   display: 'swap',
 });
 
+
 export const metadata = {
   title: 'Saksiame Solar ศักดิ์สยามโซลาร์',
   description: 'เว็บไซต์ Saksiame Solar',
   icons: {
-    icon: 'logosakico.ico',
+    icon: '/logosakico.ico',
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="th" className={sukhumvitTadmai.variable}>
-      <body className="font-sukhumvit">
+      <body className={`${sukhumvitTadmai.variable} font-sukhumvit`}>
         <LocaleProvider>
-          {/* component นี้จะใช้ client side hook แยกออกไป */}
-          <ClientConditionalLayout>{children}</ClientConditionalLayout>
+          <Navbartest />
+          <TabMenu />
+
+          <GoogleAnalytics GA_MEASUREMENT_ID="G-GRQS76P3XV" />
+          <main>{children}</main>
+
+          <ToastProvider />
+          <FloatingButtons />
+          <BackToTopButton />
+          <Footer />
+          <CookieBanner />
         </LocaleProvider>
       </body>
     </html>
