660c9ac6ac82a5269e6edd32d720e1e8e87d1b0c
Kittimaeve11 <Kittima.eve11@gmail.com>
2025-08-28 10:23:35 +0700

    Update ClientConditionalLayout.js

--- app/components/ClientConditionalLayout.js
+++ app/components/ClientConditionalLayout.js
@@ -1,38 +1,38 @@
-'use client';
+// 'use client';
 
-import { usePathname } from 'next/navigation';
-import Navbartest from './Navbartest';
-import TabMenu from './TabMenu';
-import Footer from './Footer';
-import BackToTopButton from './BackToTopButton';
-import FloatingButtons from './FloatingButtons';
-import ToastProvider from './ToastProvider';
-import CookieBanner from './CookieBanner';
-import GoogleAnalytics from './GoogleAnalytics';
-export default function ClientConditionalLayout({ children }) {
-  const pathname = usePathname();
-  const hideLayout = pathname === '/not-found'; // ปรับตาม path ที่ต้องการ
+// import { usePathname } from 'next/navigation';
+// import Navbartest from './Navbartest';
+// import TabMenu from './TabMenu';
+// import Footer from './Footer';
+// import BackToTopButton from './BackToTopButton';
+// import FloatingButtons from './FloatingButtons';
+// import ToastProvider from './ToastProvider';
+// import CookieBanner from './CookieBanner';
+// import GoogleAnalytics from './GoogleAnalytics';
+// export default function ClientConditionalLayout({ children }) {
+//   const pathname = usePathname();
+//   const hideLayout = pathname === '/not-found'; // ปรับตาม path ที่ต้องการ
 
-  return (
-    <>
-      {!hideLayout && (
-        <>
-          <Navbartest />
-          <TabMenu />
-        </>
-      )}
-      <GoogleAnalytics GA_MEASUREMENT_ID='G-GRQS76P3XV' />
-      <main>{children}</main>
+//   return (
+//     <>
+//       {!hideLayout && (
+//         <>
+//           <Navbartest />
+//           <TabMenu />
+//         </>
+//       )}
+//       <GoogleAnalytics GA_MEASUREMENT_ID='G-GRQS76P3XV' />
+//       <main>{children}</main>
 
-      {!hideLayout && (
-        <>
-          <ToastProvider />
-          <FloatingButtons />
-          <BackToTopButton />
-          <Footer />
-          <CookieBanner />
-        </>
-      )}
-    </>
-  );
-}
+//       {!hideLayout && (
+//         <>
+//           <ToastProvider />
+//           <FloatingButtons />
+//           <BackToTopButton />
+//           <Footer />
+//           <CookieBanner />
+//         </>
+//       )}
+//     </>
+//   );
+// }
